bdc82e5cdc7c476c81e220fa45819d269b44ffea
jaechick <hey@justinchick.com>
2014-09-02 14:51:01 -0500

    construct the shortcode

--- tweet-it.php
+++ tweet-it.php
@@ -13,12 +13,24 @@ License URI: http://www.wtfpl.net/
 
 class tweet_it {
 	function __construct(){
-		add_shortcode( 'tweet', array( 'tweet_it', 'tweet_func' ) );
+		add_shortcode( 'tweet-it', array( 'tweet_it', 'tweet_func' ) );
 	}
 
 	function tweet_func( $atts, $content = null ){
 		$post_id = $post->ID;
 		$permalink = get_permalink( $post_id );
 		$the_tweet = ucfirst( strip_tags( $tweet_content ) );
+
+		$ret = '<span class="tweet-it">';
+		$ret .= '<a href="https://twitter.com/intent/tweet?original_referer=' . urlencode($permalink) . '&source=tweetbutton&text=' . rawurlencode(($tweet_content)) . '&url=' . urlencode($permalink) .' >$tweet_content&thinsp;<i class="fa fa-twitter"></i>';
+		$ret .= '</a>';
+		$ret .= '<span class="sharebuttons">';
+		$ret .= '<a href="https://twitter.com/intent/tweet?original_referer=' . urlencode($permalink) . '&source=tweetbutton&text=' . rawurlencode(($tweet_content)) . '&url=' . urlencode($permalink). '">TWEET';
+		$ret .= '</a>';
+		$ret .= '</span></span>';
+
+		return $ret;
 	}
 }
+
+$tweet_it = new tweet_it();
